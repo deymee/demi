a94adbad05b5979f011facff2a85ddf3404e69a2
deymee <156134716+deymee@users.noreply.github.com>
2024-02-16 17:56:36 -0500

    assifnment 2

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name:Oluwademilade Jeleel Salami Student ID:125970228 Date: 02-16-2024
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
